fabad485f081beef6457f7b8e6082678093d5ae0
Jiyang98 <wldidska@naver.com>
2026-01-15 18:42:29 +0900

    jiyang

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Random random = new Random();
         
         // 가현 / 가은 실습 완료
-        // 제발 돼라 제발
+        // 지웠당
 
         String[] options = {"가위", "바위", "보"};
 
